3399579c1dad2d86901993189c6360de8665e943
Raphael Sena <raphars18@outlook.com>
2021-01-08 18:18:25 -0300

    Add Home Page

--- dsdeliver/front-web/src/App.tsx
+++ dsdeliver/front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import NavBar from './Navbar';
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Home />
+    </>
   );
 }
 
